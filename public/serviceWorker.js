8147ab4cf76a44d0372684b4108edc8d54d698d6
Clemens <clemens.jurkowitsch@gmx.at>
2019-12-11 12:23:35 +0100

    Updated Service Worker. Implemented Cumulative caching

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,5 +1,10 @@
 var CACHE_NAME = "site-cache";
-var urlsToCache = ["/", "/style/default.css", "/script/index.js", "/favicon.ico"];
+var urlsToCache = [
+    "/",
+    "/style/default.css",
+    "/script/index.js",
+    "/favicon.ico"
+];
 
 self.addEventListener("install", event => {
     // Perform install steps
@@ -11,14 +16,36 @@ self.addEventListener("install", event => {
     );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", function(event) {
     event.respondWith(
-        caches.match(event.request).then(response => {
+        caches.match(event.request).then(function(response) {
             // Cache hit - return response
             if (response) {
                 return response;
             }
-            return fetch(event.request);
+
+            return fetch(event.request).then(function(response) {
+                // Check if we received a valid response
+                if (
+                    !response ||
+                    response.status !== 200 ||
+                    response.type !== "basic"
+                ) {
+                    return response;
+                }
+
+                // IMPORTANT: Clone the response. A response is a stream
+                // and because we want the browser to consume the response
+                // as well as the cache consuming the response, we need
+                // to clone it so we have two streams.
+                var responseToCache = response.clone();
+
+                caches.open(CACHE_NAME).then(function(cache) {
+                    cache.put(event.request, responseToCache);
+                });
+
+                return response;
+            });
         })
     );
 });
